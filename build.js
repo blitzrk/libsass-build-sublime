1e84081f404a12b8c2a610afe2fbb09b081cf45c
Ben Krieger <blitzrk@gmail.com>
2014-10-23 12:38:37 -0600

    working; requires manual npm install

--- build.js
+++ build.js
@@ -1,11 +1,31 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
 var sass = require('node-sass');
+var path = require('path');
+var resolve = require('resolve');
+
 var file = process.argv[2];
+var settingsPath = process.argv[3];
+
 var outFile = file.substr(0, file.lastIndexOf(".")) + ".css";
+var settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8').replace(/\/\/.*|\n|\t/g,"").toString());
+var sassPaths = settings.includePaths.map(function(p) { return path.resolve('~', p) });
+
+settings.includePackages.forEach(function(m) {
+	try {
+		var res = resolve.sync(m, { basedir: process.cwd() });
+		var paths = require(res).includePaths;
+		if(!paths) throw new Error("Module " + m + " does not have an includePaths property.");
+    	sassPaths = sassPaths.concat(paths);
+	} catch(err) {
+		console.log("Warning: " + err.message.replace(/ from \'\/.*/,"."));
+	}
+});
 
 sass.renderFile({
 	"file": file,
+	includePaths: sassPaths,
 	"success": function(css) { },
 	"error": function(error){ throw new Error(error); },
 	"outFile": outFile
